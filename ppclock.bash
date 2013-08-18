6b481e190389339b635d78135743c9454644632b
Gianfrancoalongi <gianfranco.alongi@gmail.com>
2013-08-18 03:11:36 +0200

    Redesign. Will send a countdown sequence, starting from 10 to all clients.

--- ppclock.bash
+++ ppclock.bash
@@ -2,17 +2,34 @@
 
 main()
 {
-    get_active_tmux_windows
-    echo ${ACTIVE}
-}
-
-get_active_tmux_windows()
-{
-    ACTIVE=""
-    for SESSION in $(tmux list-sessions | cut -d ':' -f 1)
+    while true
     do
-	ACTIVE+=" ${SESSION}:"$(tmux list-windows -t ${SESSION} | grep \(active\) | cut -d ':' -f 1)
+	wait_until_next_swap $@
+	get_all_clients
+	send_change_message_to_all_clients
     done
 }
 
-main
+get_all_clients()
+{
+    CLIENTS=$(tmux list-clients | cut -d ':' -f 1)
+}
+
+send_change_message_to_all_clients()
+{
+    for((x=10;x>0;x--))
+    do
+	for CLIENT in ${CLIENTS}
+	do
+	    tmux display-message -c ${CLIENT} "[switch driver in: ${x} seconds]"
+	done
+	sleep 1
+    done
+}
+
+wait_until_next_swap()
+{
+    sleep ${1}
+}
+
+main $@
